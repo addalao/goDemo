6ada2b761c37f2e468dd48ef5870ce2e06915ac9
fei liu <1076435994@qq.com>
2023-11-28 16:42:05 +0800

    init接口

---
+++ internal/handler/getcontenthandler.go
@@ -0,0 +1,28 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/zeromicro/go-zero/rest/httpx"
+	"goDemo/internal/logic"
+	"goDemo/internal/svc"
+	"goDemo/internal/types"
+)
+
+func GetContentHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req types.GetContentReq
+		if err := httpx.Parse(r, &req); err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+			return
+		}
+
+		l := logic.NewGetContentLogic(r.Context(), svcCtx)
+		err := l.GetContent(&req)
+		if err != nil {
+			httpx.ErrorCtx(r.Context(), w, err)
+		} else {
+			httpx.Ok(w)
+		}
+	}
+}
