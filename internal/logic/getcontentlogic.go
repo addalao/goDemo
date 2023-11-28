26aaf2b8a027ed82535ef44c33ce26cc94732531
fei liu <1076435994@qq.com>
2023-11-28 16:19:44 +0800

    新建文件

---
+++ internal/logic/getcontentlogic.go
@@ -0,0 +1,29 @@
+package logic
+
+import (
+	"context"
+
+	"goDemo/internal/svc"
+	"goDemo/internal/types"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type GetContentLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+func NewGetContentLogic(ctx context.Context, svcCtx *svc.ServiceContext) *GetContentLogic {
+	return &GetContentLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *GetContentLogic) GetContent(req *types.GetContentReq) error {
+
+	return nil
+}
